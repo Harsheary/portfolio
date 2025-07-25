c94e9382da73979b951288384d10843553b3214c
Harsheary <harsheary2022005@gmail.com>
2025-07-25 12:27:20 -0400

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,12 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        >
         {children}
+        <Analytics />
       </body>
     </html>
+  
   );
 }
